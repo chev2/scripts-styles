65ce2b35f9bfc4ed84774183627acaa3a3c17d37
Chev <riskyrains@proton.me>
2023-07-18 18:13:22 -0700

    Twitter to Nitter UserScript: Fix indentation + bump version number

--- scripts/redirect-twitter-nitter.user.js
+++ scripts/redirect-twitter-nitter.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Redirect: Twitter to Nitter
 // @author       Chev
-// @version      1.0.0
+// @version      1.0.1
 // @description  Redirects from Twitter to Nitter, a Twitter alternative front-end.
 // @namespace    github.com/chev2
 // @license      Apache-2.0
 
-// @homepageURL    https://github.com/chev2/scripts-styles
-// @supportURL     https://github.com/chev2/scripts-styles/issues
-// @updateURL      https://raw.githubusercontent.com/chev2/scripts-styles/main/scripts/redirect-twitter-nitter.user.js
-// @downloadURL    https://raw.githubusercontent.com/chev2/scripts-styles/main/scripts/redirect-twitter-nitter.user.js
+// @homepageURL  https://github.com/chev2/scripts-styles
+// @supportURL   https://github.com/chev2/scripts-styles/issues
+// @updateURL    https://raw.githubusercontent.com/chev2/scripts-styles/main/scripts/redirect-twitter-nitter.user.js
+// @downloadURL  https://raw.githubusercontent.com/chev2/scripts-styles/main/scripts/redirect-twitter-nitter.user.js
 
 // @match        *://twitter.com/*
 // @icon         https://icons.duckduckgo.com/ip3/nitter.net.ico
